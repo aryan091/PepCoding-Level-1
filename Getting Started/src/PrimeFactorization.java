dc386d86420dafc007bb6050387f7a7b77ceb22b
ARYAN DAFTARI <59964730+aryan091@users.noreply.github.com>
2021-10-24 13:13:44 +0530

    Added 1st Module named as Getting Started
    Checkout these problems on below link
    https://www.pepcoding.com/resources/online-java-foundation/getting-started

---
+++ Getting Started/src/PrimeFactorization.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class PrimeFactorization {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n1 = sc.nextInt();
+
+
+		for (int div = 2; div < n1; div++) {
+			
+			while(n1%div==0)
+			{
+				n1=n1/div;
+				System.out.print(div+" ");
+			}
+		}
+	
+		if(n1!=1)
+		{
+			System.out.print(n1);
+		}
+		
+	}
+
+}
